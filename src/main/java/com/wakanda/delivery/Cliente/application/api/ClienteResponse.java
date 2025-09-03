f185411ffe26839bda3921b3034a63e70c1c57c8
Felipe Moraski <moraski060@gmail.com>
2025-09-03 19:34:03 -0300

    refactor(cliente): remover imports desnecessários e ajustar formatação

--- src/main/java/com/wakanda/delivery/Cliente/application/api/ClienteResponse.java
+++ src/main/java/com/wakanda/delivery/Cliente/application/api/ClienteResponse.java
@@ -1,4 +1,25 @@
 package com.wakanda.delivery.Cliente.application.api;
 
+import com.wakanda.delivery.Cliente.domain.Cliente;
+
+import java.util.UUID;
+
 public class ClienteResponse {
+
+
+     UUID idCliente;
+     String nome;
+     String cpf;
+     String email;
+     String telefone;
+     String endereco;
+
+    public ClienteResponse(Cliente cliente){
+        this.idCliente = cliente.getIdCliente();
+        this.nome = cliente.getNome();
+        this.cpf = cliente.getCpf();
+        this.email = cliente.getEmail();
+        this.telefone = cliente.getTelefone();
+        this.endereco = cliente.getEndereco();
+    }
 }
--- src/main/java/com/wakanda/delivery/Cliente/application/api/CriarClienteRequest.java
+++ src/main/java/com/wakanda/delivery/Cliente/application/api/CriarClienteRequest.java
@@ -1,4 +1,10 @@
 package com.wakanda.delivery.Cliente.application.api;
 
 public class CriarClienteRequest {
+
+    String nome;
+    String cpf;
+    String email;
+    String telefone;
+    String endereco;
 }
