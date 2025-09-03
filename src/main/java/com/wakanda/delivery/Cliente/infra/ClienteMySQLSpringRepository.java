8855052c2ddf7db5b155866c03ce0a45b79e84e9
Felipe Moraski <moraski060@gmail.com>
2025-09-03 20:49:38 -0300

    feat: Criar ClienteRepositoryMySQL em infra, e corrigir ClienteMySQLRepository

--- src/main/java/com/wakanda/delivery/Cliente/infra/ClienteMySQLSpringRepository.java
+++ src/main/java/com/wakanda/delivery/Cliente/infra/ClienteMySQLSpringRepository.java
@@ -1,4 +1,9 @@
 package com.wakanda.delivery.Cliente.infra;
 
-public interface ClienteMySQLSpringRepository {
+import com.wakanda.delivery.Cliente.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClienteMySQLSpringRepository extends JpaRepository<Cliente, UUID> {
 }
