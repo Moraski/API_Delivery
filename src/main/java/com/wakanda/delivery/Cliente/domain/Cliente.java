6185f6ad945a56c5cebb893481c1328852e5d5c2
Felipe Moraski <moraski060@gmail.com>
2025-09-03 20:09:04 -0300

    feat: adicionar construtor que inicializa Cliente a partir de ClienteNovoRequest

--- src/main/java/com/wakanda/delivery/Cliente/domain/Cliente.java
+++ src/main/java/com/wakanda/delivery/Cliente/domain/Cliente.java
@@ -1,6 +1,8 @@
 package com.wakanda.delivery.Cliente.domain;
 
+import com.wakanda.delivery.Cliente.application.api.ClienteNovoRequest;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.*;
 
 import java.util.UUID;
@@ -32,4 +34,12 @@ public class Cliente {
 
     @Column(nullable = false)
     private String endereco;
+
+    public Cliente(@Valid ClienteNovoRequest clienteNovo) {
+        this.nome = clienteNovo.getNome();
+        this.cpf = clienteNovo.getCpf();
+        this.email = clienteNovo.getEmail();
+        this.telefone = clienteNovo.getTelefone();
+        this.endereco = clienteNovo.getEndereco();
+    }
 }
