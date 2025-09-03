e39c81a6b62d90e4d289f2467da398a84dcf3939
Felipe Moraski <moraski060@gmail.com>
2025-09-03 17:58:06 -0300

    chore: setup inicial do projeto Spring Boot + Docker Compose

---
+++ src/main/java/com/wakanda/delivery/ProjetoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wakanda/delivery/ProjetoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakanda.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
