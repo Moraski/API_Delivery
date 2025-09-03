8b54314351e8ecf900097b662acacf6b90c5ddf6
Felipe Moraski <moraski060@gmail.com>
2025-09-03 19:45:47 -0300

    feat: Criar ClienteRepository em repository

---
+++ src/main/java/com/wakanda/delivery/Cliente/application/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.wakanda.delivery.Cliente.application.repository;
+
+import com.wakanda.delivery.Cliente.domain.Cliente;
+
+public interface ClienteRepository {
+
+    Cliente salva(Cliente cliente);
+}
