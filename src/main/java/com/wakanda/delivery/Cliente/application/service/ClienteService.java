6a9f040b0feb2e45c57d9e15eb32eb6213420b27
Felipe Moraski <moraski060@gmail.com>
2025-09-05 15:21:25 -0300

    feat: implementa camada Service

--- src/main/java/com/wakanda/delivery/Cliente/application/service/ClienteService.java
+++ src/main/java/com/wakanda/delivery/Cliente/application/service/ClienteService.java
@@ -3,8 +3,13 @@ package com.wakanda.delivery.Cliente.application.service;
 import com.wakanda.delivery.Cliente.application.api.ClienteResponse;
 import com.wakanda.delivery.Cliente.application.api.ClienteNovoRequest;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
 
 public interface ClienteService {
 
      ClienteResponse criaNovoCliente(@Valid ClienteNovoRequest cliente);
+
+     ClienteResponse buscarClientePorId(@PathVariable UUID idCliente);
 }
