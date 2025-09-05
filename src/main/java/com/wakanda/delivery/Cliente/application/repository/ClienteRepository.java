e978d33539637d060a5d7f31b5382d82b00638af
Felipe Moraski <moraski060@gmail.com>
2025-09-05 14:42:32 -0300

    feat:implementa busca de cliente por ID (repository + infra)

--- src/main/java/com/wakanda/delivery/Cliente/application/repository/ClienteRepository.java
+++ src/main/java/com/wakanda/delivery/Cliente/application/repository/ClienteRepository.java
@@ -2,7 +2,11 @@ package com.wakanda.delivery.Cliente.application.repository;
 
 import com.wakanda.delivery.Cliente.domain.Cliente;
 
+import java.util.UUID;
+
 public interface ClienteRepository {
 
     Cliente salva(Cliente cliente);
+
+    Cliente buscaPorId(UUID idCliente);
 }
--- src/main/java/com/wakanda/delivery/Cliente/infra/ClienteRepositoryMySQL.java
+++ src/main/java/com/wakanda/delivery/Cliente/infra/ClienteRepositoryMySQL.java
@@ -4,7 +4,10 @@ import com.wakanda.delivery.Cliente.application.repository.ClienteRepository;
 import com.wakanda.delivery.Cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,5 +22,14 @@ public class ClienteRepositoryMySQL implements ClienteRepository {
         Cliente novoCliente = clienteMySQLSpringRepository.save(cliente);
         log.info("[Finaliza] ClienteRepositoryMySQL - salva");
         return novoCliente;
+    }
+
+    @Override
+    public Cliente buscaPorId(UUID idCliente) {
+        log.info("[Inicia] ClienteRepositoryMySQL - buscaPorId");
+        Cliente cliente = clienteMySQLSpringRepository.findById(idCliente)
+                        .orElseThrow(() -> new APIException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+        log.info("[Inicia] ClienteRepositoryMySQL - buscaPorId");
+        return cliente;
     }
 }
