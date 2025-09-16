f1817bb8ad0f4a4e1a28f470324fe98b0ebebb82
Felipe Moraski <moraski060@gmail.com>
2025-09-16 17:58:02 -0300

    feat: Implementação da interface ItemRepository em repository

--- src/main/java/com/wakanda/delivery/itens/application/repository/ItemRepository.java
+++ src/main/java/com/wakanda/delivery/itens/application/repository/ItemRepository.java
@@ -1,4 +1,8 @@
 package com.wakanda.delivery.itens.application.repository;
 
+import com.wakanda.delivery.itens.domain.Itens;
+
 public interface ItemRepository {
+
+    Itens salva(Itens itens);
 }
