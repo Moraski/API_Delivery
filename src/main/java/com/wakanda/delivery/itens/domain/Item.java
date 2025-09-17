7ec536e1125d52c04cb4cd8e9f8cf23635f87420
Felipe Moraski <moraski060@gmail.com>
2025-09-17 16:14:41 -0300

    feat: criação do construtor editaItem em Item

--- src/main/java/com/wakanda/delivery/itens/domain/Item.java
+++ src/main/java/com/wakanda/delivery/itens/domain/Item.java
@@ -1,9 +1,16 @@
 package com.wakanda.delivery.itens.domain;
 
+import com.wakanda.delivery.handler.APIException;
 import com.wakanda.delivery.itens.application.api.ItemNovoRequest;
+import com.wakanda.delivery.itens.application.api.ItemResponse;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 @Entity
@@ -38,5 +45,15 @@ public class Item {
         this.valorUnitario = itemNovo.getValorUnitario();
         this.quantidade = itemNovo.getQuantidade();
         this.descricao = itemNovo.getDescricao();
+    }
+
+    public void editaItem(String produto, Double valorUnitario, Integer quantidade, String descricao){
+        if (produto == null || produto.isBlank()){
+            throw APIException.build(HttpStatus.BAD_REQUEST, "produto não pode estar vazio");
+        }
+        this.produto = produto;
+        this.valorUnitario = valorUnitario;
+        this.quantidade = quantidade;
+        this.descricao = descricao;
     }
 }
