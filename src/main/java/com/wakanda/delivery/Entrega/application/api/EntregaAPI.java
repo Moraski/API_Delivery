3aab5d89728e08ac504ff4963311c7ec9736c915
Felipe Moraski <moraski060@gmail.com>
2025-09-22 19:23:12 -0300

    feat: Criar a interface EntregaAPI e criar o método GetEntregaById em EntregaAPI

--- src/main/java/com/wakanda/delivery/Entrega/application/api/EntregaAPI.java
+++ src/main/java/com/wakanda/delivery/Entrega/application/api/EntregaAPI.java
@@ -1,4 +1,17 @@
 package com.wakanda.delivery.Entrega.application.api;
 
+
+import com.wakanda.delivery.Cliente.application.api.ClienteNovoRequest;
+import com.wakanda.delivery.Cliente.application.api.ClienteResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping(value = "/public/v1/Entrega")
 public interface EntregaAPI {
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    ClienteResponse GetEntregaById(@PathVariable UUID IdEntrega);
 }
--- src/main/java/com/wakanda/delivery/Pedido/domain/Pedido.java
+++
@@ -1,4 +0,0 @@
-package com.wakanda.delivery.Pedido.domain;
-
-public class Pedido {
-}
