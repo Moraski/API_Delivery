647bca96a5872ca6165fe0a3644290c4d76c5357
Felipe Moraski <moraski060@gmail.com>
2025-09-22 19:54:24 -0300

    feat: Cria Interface EntregaRepository em repository

---
+++ src/main/java/com/wakanda/delivery/Entrega/application/repository/EntregaRepository.java
@@ -0,0 +1,8 @@
+package com.wakanda.delivery.Entrega.application.repository;
+
+import com.wakanda.delivery.Entrega.domain.Entrega;
+
+public interface EntregaRepository {
+
+    Entrega salva(Entrega entrega);
+}
