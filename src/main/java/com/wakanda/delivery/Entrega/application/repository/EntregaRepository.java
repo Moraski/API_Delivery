07e346dfec6f2d552233d91562377bffaa6cc5ff
Felipe Moraski <moraski060@gmail.com>
2025-09-22 20:00:39 -0300

    refactor: correções em EntregaRepository

--- src/main/java/com/wakanda/delivery/Entrega/application/repository/EntregaRepository.java
+++ src/main/java/com/wakanda/delivery/Entrega/application/repository/EntregaRepository.java
@@ -1,8 +1,11 @@
 package com.wakanda.delivery.Entrega.application.repository;
 
+import com.wakanda.delivery.Cliente.domain.Cliente;
 import com.wakanda.delivery.Entrega.domain.Entrega;
+
+import java.util.UUID;
 
 public interface EntregaRepository {
 
-    Entrega salva(Entrega entrega);
+    Cliente buscaPorId(UUID idCliente);
 }
