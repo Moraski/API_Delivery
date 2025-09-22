c96a188dfc0344025de5a64d0b6856ba317880f7
Felipe Moraski <moraski060@gmail.com>
2025-09-22 18:10:27 -0300

    adicionar entidade ItemDoPedido

---
+++ src/main/java/com/wakanda/delivery/itens/domain/ItemDoPedido.java
@@ -0,0 +1,36 @@
+package com.wakanda.delivery.pedido.domain;
+
+import com.wakanda.delivery.itens.domain.Item;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "pedido_itens")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ItemDoPedido {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_pedido", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_item", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    public Double getSubtotal() {
+        return item.getValorUnitario() * quantidade;
+    }
+}
