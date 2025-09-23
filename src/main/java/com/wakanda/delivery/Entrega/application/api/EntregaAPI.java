4914b5fd9e191811ae3611906e7fc6965bae8675
Felipe Moraski <moraski060@gmail.com>
2025-09-23 18:52:43 -0300

    feat: Cria o método UpdateEntreega em EntregaAPI

--- src/main/java/com/wakanda/delivery/Entrega/application/api/EntregaAPI.java
+++ src/main/java/com/wakanda/delivery/Entrega/application/api/EntregaAPI.java
@@ -2,6 +2,7 @@ package com.wakanda.delivery.Entrega.application.api;
 
 
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +10,12 @@ import java.util.UUID;
 
 @RequestMapping(value = "/public/v1/Entrega")
 public interface EntregaAPI {
+
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     EntregaResponse GetEntregaById(@PathVariable UUID IdEntrega);
+
+    @PutMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    EntregaResponse UpdateEntrega(@PathVariable UUID idEntrega, @RequestBody @Valid EditaEntregaRequest editaEntregaRequest);
 }
